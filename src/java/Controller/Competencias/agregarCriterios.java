d8c1f0934804c4432b47a7a0fff3abaa5df0476e
Carlos Guillermo Bolanos Lopez <Garrison@Underground.local>
2010-11-21 21:35:59 -0600

    competencias 90%

--- src/java/Controller/Competencias/agregarCriterios.java
+++ src/java/Controller/Competencias/agregarCriterios.java
@@ -73,6 +73,7 @@ public class agregarCriterios extends HttpServlet {
         
         try {
                 CriterioCompetencia = cc.obtenerCriterioxCompetencia(idCompetencia);
+                //Criterios = c.obtenerCriteriosxCompetencia(idCompetencia);
                 Criterios = c.obtenerCriterios();
                 if(CriterioCompetencia != null){                    
                     request.setAttribute("CriterioCompetencia",CriterioCompetencia );
